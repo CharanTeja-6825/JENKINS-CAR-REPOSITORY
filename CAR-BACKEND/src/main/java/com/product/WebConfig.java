5aeb8a925b8922be40d6225cc72c4df24a299273
Rathikindi Charan Teja <rathikindicharanteja@Rathikindis-MacBook-Air.local>
2025-10-14 00:05:00 +0530

    Project v1.1

--- CAR-BACKEND/src/main/java/com/product/WebConfig.java
+++ CAR-BACKEND/src/main/java/com/product/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // your React app URL
+                        .allowedOrigins("http://localhost:2030") // your React app URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowCredentials(true);
             }
